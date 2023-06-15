37d5d0fc2d9eca050678b5dbb0245cd03bca2f99
JoaoM-py <joao.marcos2002@hotmail.com>
2023-06-14 21:46:13 -0300

    Adding information on .env.sample

--- src/modules/mailer/mailer.module.ts
+++ src/modules/mailer/mailer.module.ts
@@ -8,22 +8,17 @@ import { MailService } from './mailer.service';
   imports: [
     MailerModule.forRoot({
       transport: {
-        host: 'smtp.mailgun.org', //host smtp
+        host: process.env.HOST_MAILGUN, //host smtp
         secure: false, //regras de segurança do serviço smtp
         port: 587, // porta
         auth: { //dados do usuário e senha
-          user: 'postmaster@sandboxa9d6ddbf0bf248b88d53fb005c699239.mailgun.org',
-          pass: '0256c9e764500285703605000e3b1d13-af778b4b-7d50a1b5',
+          user: process.env.USER_MAILGUN,
+          pass: process.env.PASSWORD_MAILGUN,
         },
         ignoreTLS: true,
       },
       defaults: { // configurações que podem ser padrões
         from: '"',
-      },
-      template:{
-        options:{
-          name:'alert'
-        }
       }
     }),
   ],
